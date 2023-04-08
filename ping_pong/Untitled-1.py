fcd95f3cfb95aeb3befe6ce72db4f1914474876e
liopzz <128907838+liopzz@users.noreply.github.com>
2023-04-08 19:39:46 +0500

    Update Untitled-1.py

--- ping_pong/Untitled-1.py
+++ ping_pong/Untitled-1.py
@@ -24,31 +24,56 @@ class Player2(GameSprite):
         if keys[K_DOWN] and self.rect.y<510:
             self.rect.y +=8
 
+
 window = display.set_mode((800,600))
 
 display.set_caption('ping_pong')
 background = transform.scale(image.load('bg.png'),(800,600))
 player1 = Player('platformm.png',0,210,30,90)
 player2 = Player2('platformm.png',770,210,30,90)
-
+ball = GameSprite('balls.png',200,200,50,50)
 clock = time.Clock()
 FPS = 60
 
-
+speed_x = 3
+speed_y = 3
 finish =False
 run = True
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 while run:
-    window.blit(background,(0,0))
-    player1.reset()
-    player1.update()
-    player2.reset()
-    player2.update()
     for e in event.get():
         if e.type == QUIT:
             run= False
-        
-
-    clock.tick(FPS)
+    if finish == False:
+        window.blit(background,(0,0))
+        player1.reset()
+        player1.update()
+        player2.reset()
+        player2.update()
+    
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+  
+    
+    if sprite.collide_rect(player1,ball) or sprite.collide_rect(player2,ball):
+        speed_x *= -1
+        speed_y *= 1
+    if ball.rect.y >550 or ball.rect.y <0:
+        speed_y *= -1
+    if ball.rect.x <0:
+        finish= True
+        window.blit(lose1,(300,250))
+        game_over = True
+    if ball.rect.x > 800:
+        finish = True
+        window.blit(lose2,(300,250))
     display.update()
+    clock.tick(FPS)
+    
+
 
 
